76bdbfca0a5cf28ef2b44e446f4a17e903532047
n0remac <smartcameron@gmail.com>
2020-08-30 19:14:44 -0700

    Added constants for screen and sprite size.

--- start_game.py
+++ start_game.py
@@ -9,9 +9,24 @@ from Graphics.PostEffects.TrashChromaticAberration import TrashChromaticAberrati
 
 SCREEN_TITLE = "PyMunk Platformer"
 
+# How big are our image tiles?
+SPRITE_IMAGE_SIZE = 128
+
+# Scale sprites up or down
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_TILES = 0.5
+
+# Scaled sprite size for tiles
+SPRITE_SIZE = int(SPRITE_IMAGE_SIZE * SPRITE_SCALING_PLAYER)
+
+# Size of grid to show on screen, in number of tiles
+SCREEN_GRID_WIDTH = 25
+SCREEN_GRID_HEIGHT = 15
+
 # Size of screen to show, in pixels
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = SPRITE_SIZE * SCREEN_GRID_WIDTH
+SCREEN_HEIGHT = SPRITE_SIZE * SCREEN_GRID_HEIGHT
+
 
 
 class GameWindow(arcade.Window):
